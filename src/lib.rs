c42541811edd91b05cf911e25de404519f05ad5c
Oliver Scherer <github@oli-obk.de>
2023-11-30 13:43:51 +0100

    Add field for setting the user agent

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,4 @@ mod web_asset_plugin;
 mod web_asset_source;
 
 pub use web_asset_plugin::WebAssetPlugin;
-pub use web_asset_source::WebAssetReader;
+pub use web_asset_source::{WebAssetReader, WebAssetReaderKind};
--- src/web_asset_plugin.rs
+++ src/web_asset_plugin.rs
@@ -21,17 +21,28 @@ use bevy::asset::io::AssetSource;
 /// ));
 /// ```
 #[derive(Default)]
-pub struct WebAssetPlugin;
+pub struct WebAssetPlugin {
+    /// User agent to tell the server about. Some servers require this to be set.
+    /// Note: This flag is entirely ignored on wasm, as only firefox reliably supports this.
+    pub user_agent: Option<String>,
+}
 
 impl Plugin for WebAssetPlugin {
     fn build(&self, app: &mut App) {
-        app.register_asset_source(
-            "http",
-            AssetSource::build().with_reader(|| Box::new(WebAssetReader::Http)),
-        );
-        app.register_asset_source(
-            "https",
-            AssetSource::build().with_reader(|| Box::new(WebAssetReader::Https)),
-        );
+        for (id, kind) in [
+            ("http", WebAssetReaderKind::Http),
+            ("https", WebAssetReaderKind::Https),
+        ] {
+            let user_agent = self.user_agent.clone();
+            app.register_asset_source(
+                id,
+                AssetSource::build().with_reader(move || {
+                    Box::new(WebAssetReader {
+                        user_agent: user_agent.clone(),
+                        kind,
+                    })
+                }),
+            );
+        }
     }
 }
