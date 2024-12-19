e27e4657ade6a529a8aeb33d6116544040891fc9
mosure <mitchell@mosure.me>
2024-12-19 12:04:15 -0600

    fix: lint

--- examples/web_image.rs
+++ examples/web_image.rs
@@ -16,7 +16,7 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d::default());
 
-    commands.spawn(
-        Sprite::from_image(asset_server.load("https://s3.johanhelsing.studio/dump/favicon.png"))
-    );
+    commands.spawn(Sprite::from_image(
+        asset_server.load("https://s3.johanhelsing.studio/dump/favicon.png")
+    ));
 }
